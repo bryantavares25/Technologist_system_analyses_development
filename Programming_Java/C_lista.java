9661522823e348aa78d4580e112401f7fd3ce2e3
Bryan Tavares <bryantavares25@gmail.com>
2023-11-12 15:23:39 -0300

    Create C_lista.java

---
+++ Programming_Java/C_lista.java
@@ -0,0 +1,25 @@
+public class C_lista {
+    
+    public static void main(String[] args) {
+        System.out.println("H E L L O W O R L D ! ! !");
+
+
+    }
+
+    public class MeusArrays {
+    
+        // Propriedades
+        public Array ar01; 
+        public Array ar02;
+        public int num;
+        
+        // Métodos
+
+
+    }   
+
+    public class TesteArrays {
+    
+        
+    }
+}
