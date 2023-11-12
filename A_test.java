79f5bda9531f355dddae3dcfff85b716131128d2
Bryan Tavares <bryantavares25@gmail.com>
2023-11-12 14:24:24 -0300

    Updates

---
+++ A_test.java
@@ -0,0 +1,6 @@
+public class A_test{
+    
+    public static void main(String[] args) {
+        System.out.println("H E L L O W O R L D ! ! !");
+    }
+}
---
+++ testjava.java
@@ -0,0 +1,6 @@
+public class testjava {
+
+    public static void main(String[] args) {
+        System.out.println("H E L L O W O R L D ! ! !");
+    }
+}
