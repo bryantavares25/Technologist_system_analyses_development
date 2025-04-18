190bd0fc424649d08347023973c9f58055836d9e
Bryan Tavares <bryan.ar.tavares@gmail.com>
2025-04-18 19:12:03 -0300

    Update main.dart

--- Aula_widgets/flutter_application_2/lib/main.dart
+++ Aula_widgets/flutter_application_2/lib/main.dart
@@ -6,24 +6,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   //runApp(const MyApp());
-  runApp(MeuAplicativo());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  Widget build (Contexto BuildContext) {
-    return MaterialApp(:
-    Scaffold(
-      appBar: AppBar(
-        title: Text("Exploring Widgets"),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Exploring Widgets")),
+        body: myWidget(),
       ),
-      corpo: meuWidget(),
-    ),
     );
   }
 }
 
+//Widget myWidget() {
+//return Center(
+//
+//  child: Text("Hello world! - Olá mundo!", style: TextStyle(fontSize: 30)),
+//);
+//return Padding(padding: EdgeInsets.all(16.0), child: Text("Olá, Flutter!"));
+//}
+
 Widget myWidget() {
-  return Text(
-    "Hello, World!",
+  return ElevatedButton(
+    onPressed: () {
+      // Ação quando o botão for pressionado
+    },
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.blue, // Substitui "color"
+      elevation: 4.0, // Substitui "altitude"
+      splashFactory: InkSplash.splashFactory, // Alternativa ao splashColor
+    ),
+    child: const Text('Button'),
   );
 }
