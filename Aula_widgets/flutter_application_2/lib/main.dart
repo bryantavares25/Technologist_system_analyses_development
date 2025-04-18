d011874832ff9a5f5a92887b3cca18f0e061c056
Bryan Tavares <bryan.ar.tavares@gmail.com>
2025-04-18 18:31:10 -0300

    Update main.dart

--- Aula_widgets/flutter_application_2/lib/main.dart
+++ Aula_widgets/flutter_application_2/lib/main.dart
@@ -1,8 +1,29 @@
 // Flutter and Dart
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
   //runApp(const MyApp());
-  runApp(Container(color: Colors.orangeAccent));
+  runApp(MeuAplicativo());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build (Contexto BuildContext) {
+    return MaterialApp(:
+    Scaffold(
+      appBar: AppBar(
+        title: Text("Exploring Widgets"),
+      ),
+      corpo: meuWidget(),
+    ),
+    );
+  }
+}
+
+Widget myWidget() {
+  return Text(
+    "Hello, World!",
+  );
 }
