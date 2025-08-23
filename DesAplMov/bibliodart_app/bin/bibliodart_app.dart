cdf9e60662eb9784e56de5384a580de5b330ef0a
Bryan Tavares <bryan.ar.tavares@gmail.com>
2025-08-23 20:31:22 -0300

    Update

--- DesAplMov/bibliodart_app/bin/bibliodart_app.dart
+++ DesAplMov/bibliodart_app/bin/bibliodart_app.dart
@@ -1,5 +1,22 @@
 import 'package:bibliodart_app/bibliodart_app.dart' as bibliodart_app;
 
+class Livro {
+  //Atributos
+  String titulo;
+  String autor;
+  int anoPublicacao;
+
+  Livro(this.titulo, this.autor, this.anoPublicacao);
+
+  void exibirdetalhes() {
+    print("=== Detalhes do Livro ===");
+    print('Título: $titulo');
+    print('Autor: $autor');
+    print('Ano de Publicação: $anoPublicacao');
+    print("==========================");
+  }
+}
+
 void main(List<String> arguments) {
   print('Hello world: ${bibliodart_app.calculate()}!');
 }
