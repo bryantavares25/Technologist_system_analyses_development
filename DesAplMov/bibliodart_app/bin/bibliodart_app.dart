66fcbbd124f2f03643454f586f39f4e1244c6b8d
Bryan Tavares <bryan.ar.tavares@gmail.com>
2025-08-23 19:53:31 -0300

    Update

---
+++ DesAplMov/bibliodart_app/bin/bibliodart_app.dart
@@ -0,0 +1,5 @@
+import 'package:bibliodart_app/bibliodart_app.dart' as bibliodart_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bibliodart_app.calculate()}!');
+}
---
+++ DesAplMov/bibliodart_app/test/bibliodart_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:bibliodart_app/bibliodart_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
