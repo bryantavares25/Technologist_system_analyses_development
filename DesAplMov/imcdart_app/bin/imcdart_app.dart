4f8ee3bdfc9b13f17699aa7085af348b1225cf25
Bryan Tavares <bryan.ar.tavares@gmail.com>
2025-08-24 09:52:31 -0300

    New project

---
+++ DesAplMov/imcdart_app/bin/imcdart_app.dart
@@ -0,0 +1,5 @@
+import 'package:imcdart_app/imcdart_app.dart' as imcdart_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imcdart_app.calculate()}!');
+}
---
+++ DesAplMov/imcdart_app/test/imcdart_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:imcdart_app/imcdart_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
